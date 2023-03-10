27022de201372fa4020898da6f25cf5a6401ac58
felix.ravella <felix.ravella@gmail.com>
2023-03-10 14:54:36 +0100

    feat: add button with alert on tap

--- components/CountButton.js
+++ components/CountButton.js
@@ -1,7 +1,21 @@
-import { Text } from "react-native";
+import { Alert, Button, Text, View } from "react-native";
 
 export default function CountButton() {
+
+  function testButton(){
+    Alert.alert('Un test d\'alert')
+  }
+
   return (
+    <View>
       <Text>Du text</Text>
+      <Button
+        onPress={testButton}
+        title="Learn More"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+      />
+    </View>
+      
   );
 }
